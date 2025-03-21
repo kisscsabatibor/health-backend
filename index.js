8b6d542cd6a479439076b701ccb0afd105fec8cc
Kiss Csaba Tibor <kisscsabatibor@protonmail.com>
2025-03-21 08:08:45 +0100

    MongoDB connection

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const express = require('express')
+import express from 'express'
+import { config } from 'dotenv'
+import { MongoClient } from 'mongodb'
 const app = express()
 const port = 3000
 
@@ -6,6 +8,31 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  config()
+  const uri = process.env.DB_URI
+  let mongoClient
+
+  try {
+    mongoClient = await connectToCluster(uri)
+  } finally {
+    await mongoClient.close()
+  }
   console.log(`Example app listening on port ${port}`)
 })
+
+export async function connectToCluster(uri) {
+  let mongoClient
+
+  try {
+    mongoClient = new MongoClient(uri)
+    console.log('Connecting to MongoDB Atlas cluster...')
+    await mongoClient.connect()
+    console.log('Successfully connected to MongoDB Atlas!')
+
+    return mongoClient
+  } catch (error) {
+    console.error('Connection to MongoDB Atlas failed!', error)
+    process.exit()
+  }
+}
