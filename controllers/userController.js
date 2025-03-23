edf57aab9be554aa4c5a6cc2b7fb29fef1e6536c
Kiss Csaba Tibor <kisscsabatibor@protonmail.com>
2025-03-23 11:12:34 +0100

    User can update profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,3 +17,21 @@ export const deleteUser = async (req, res) => {
     res.status(500).json({ error: err.message })
   }
 }
+
+export const updateProfile = async (req, res) => {
+  try {
+    const updates = req.body
+    const user = await User.findByIdAndUpdate(req.user.userId, updates, {
+      new: true,
+      runValidators: true,
+    }).select('-password')
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' })
+    }
+
+    res.json(user)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express'
-import { deleteUser, getProfile } from '../controllers/userController.js'
+import {
+  deleteUser,
+  getProfile,
+  updateProfile,
+} from '../controllers/userController.js'
 import authMiddleware from '../middleware/auth.js'
 
 const router = express.Router()
 
 router.get('/profile', authMiddleware, getProfile)
 router.delete('/profile', authMiddleware, deleteUser)
+router.put('/profile', authMiddleware, updateProfile)
 
 export default router
